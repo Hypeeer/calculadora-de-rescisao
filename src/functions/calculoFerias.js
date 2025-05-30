bfae6bb4f6f37640bf978a25a29a0407c2ffe78a
Hypeeer <vittorjoao18@hotmail.com>
2025-05-29 21:16:21 -0300

    init: projeto inicial

--- src/functions/calculoFerias.js
+++ src/functions/calculoFerias.js
@@ -1,6 +1,9 @@
 const feriasVencidas = (salario, periodo) => {
-  let valor = (salario / 12) * periodo;
-  return valor;
+  const valor = (salario / 12) * periodo;
+  const proporcional = valor * (1 / 3);
+  const valorTotal = valor + proporcional;
+
+  return valorTotal;
 };
 
 console.log(feriasVencidas(3000, 12));
